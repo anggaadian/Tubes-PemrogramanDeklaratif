71b1545151f0a251326b345fe3f2550e3ddf8381
anggitpangess <64422364+anggitpangess@users.noreply.github.com>
2020-05-01 04:14:56 +0700

    Add files via upload

---
+++ Project Tubes Impelentasi  Prolog Ke dalam Bahasa Pemrograman PHP/indexboostrap.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="zxx">
+<head>
+	<title>Tubes Pemdek</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="SolMusic HTML Template">
+	<meta name="keywords" content="music, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	
+	<!-- Favicon -->
+
+
+	<!-- Google font -->
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,400i,500,500i,600,600i,700,700i&display=swap" rel="stylesheet">
+ 
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.min.css"/>
+	<link rel="stylesheet" href="css/slicknav.min.css"/>
+
+	<!-- Main Stylesheets -->
+	<link rel="stylesheet" href="css/style.css"/>
+
+
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+	<!-- Header section -->
+	<header class="header-section clearfix">
+		<a href="index.html" class="site-logo">
+		
+		</a>
+		
+		<ul class="main-menu">
+			<li><a href="modul1-family/index.php">Modul 1</a></li>
+			<li><a href="modul2-manipulasilist/index.php">Modul 2</a></li>
+			<li><a href="Modul3-aritmatika/index.php">Modul 3</a>
+				
+			</li>
+			<li><a href="Modul4-operator/index.php">Modul 4</a></li>
+			<li><a href="#">Modul 5</a></li>
+				<li><a href="#">Modul 6</a></li>
+		</ul>
+	</header>
+	<!-- Header section end -->
+
+	<!-- Hero section -->
+	<section class="hero-section">
+		<div class="hero-slider owl-carousel">
+			<div class="hs-item">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-6">
+							<div class="hs-text">
+								<h2><span>Pemrograman</span> Deklaratif</h2>
+								<a href="https://github.com/anggaadian/Tubes-PemrogramanDeklaratif" class="site-btn">Link Sistem Pakar</a>
+								<a href="https://github.com/anggaadian/Tubes-PemrogramanDeklaratif" class="site-btn">Link Video</a>
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="hr-img">
+								<img src="img/hero-bg.png" alt="">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="hs-item">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-6">
+							<div class="hs-text">
+								<h2><span>Anggota </span> Kelompok</h2>
+								<p><p>Angga Dian Permana P [190535746102], Anggit Pangestu [[190535746100], Hefi Ratna N[190535746101], Moza Nailur R [190535746103], Sayed R H[180535600764] </p>
+						<a href="#" class="site-btn">Link Implementasi PHP</a>
+						<a href="#" class="site-btn">Link Video</a>
+								
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<div class="hr-img">
+								<img src="img/hero-bg.png" alt="">
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Hero section end -->
+
+	<!-- Intro section -->
+	
+	<!-- Intro section end -->
+
+	<!-- How section -->
+	<section class="how-section spad set-bg" data-setbg="img/how-to-bg.jpg">
+		<div class="container text-white">
+			<div class="section-title">
+				<h2>How it works</h2>
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+					<div class="how-item">
+						<div class="hi-icon">
+							<img src="img/icons/brain.png" alt="">
+						</div>
+						<h4>Create an Github account</h4>
+						<p> Langkah pertama membuat akun Github dengan email yang dipunyai </p>
+						</div>
+				</div>
+				<div class="col-md-4">
+					<div class="how-item">
+						<div class="hi-icon">
+							<img src="img/icons/pointer.png" alt="">
+						</div>
+						<h4>Download Github's Project</h4>
+						<p> Download Github project kelompok kami dengan link berikut https://github.com/anggaadian/Tubes-PemrogramanDeklaratif </p>			</div>
+				</div>
+				<div class="col-md-4">
+					<div class="how-item">
+						<div class="hi-icon">
+							<img src="img/icons/smartphone.png" alt="">
+						</div>
+						<h4>Use on your PC</h4>
+						<p> Project siap untuk digunakan </p></div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	
+	<section class="premium-section spad">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6">
+					<div class="section-title">
+						<h2>Tentang apa project ini?</h2>
+					</div>
+				</div>
+				<div class="col-lg-6">
+					<p> Pada pengerjaan tugas besar ini merupakan Implementasi dari Prolog ke bahasa pemrograman PHP. Selain
+					itu kami menyajikan Program Sistem Pakar Hama Padi. Terdapat sajian sebagai berikut  </p></div>
+			</div>
+			<div class="row">
+				<div class="col-lg-3 col-sm-6">
+					<div class="premium-item">
+						<img src="img/premium/1.jpg" alt="">
+						<h4><a href="modul1-family/index.php">Modul 1</a></h4>
+						<h4><a href="modul2-manipulasilist/index.php">Modul 2</a></h4>
+						<p>Family Tree dan Manipulasi List </p>
+					</div>
+				</div>
+				<div class="col-lg-3 col-sm-6">
+					<div class="premium-item">
+						<img src="img/premium/2.jpg" alt="">
+						<h4><a href="modul3/index.php">Modul 3</a></h4>
+						<p>Operasi Aritmatika</p>
+					</div>
+				</div>
+				<div class="col-lg-3 col-sm-6">
+					<div class="premium-item">
+						<img src="img/premium/3.jpg" alt="">
+						<h4><a href="modul4/index.php">Modul 4</a></h4>
+						<p> Operasi Logika </p>
+					</div>
+				</div>
+				<div class="col-lg-3 col-sm-6">
+					<div class="premium-item">
+						<img src="img/premium/4.jpg" alt="">
+						<h4><a href="modul5/index.php">Modul 5</a></h4>
+						<h4><a href="modul6/index.php">Modul 6</a></h4>
+						<p>List of List dan Binary Tree</p>
+					</div>
+					
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Premium section end -->
+
+	<!-- Footer section -->
+	<footer class="footer-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-xl-6 col-lg-7 order-lg-2">
+					<div class="row">
+						
+						</div>
+					</div>
+				</div>
+				<div class="col-xl-6 col-lg-5 order-lg-1">
+					
+					<div class="copyright"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></div>
+					
+				</div>
+			</div>
+		</div>
+	</footer>
+	<!-- Footer section end -->
+	
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/jquery.slicknav.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/mixitup.min.js"></script>
+	<script src="js/main.js"></script>
+
+	</body>
+</html>
